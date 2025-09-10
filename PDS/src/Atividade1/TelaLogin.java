d1d651e77ceffef2ce3c9f040287e6b6f46f0f7f
emily <emily.s23@aluno.ifsc.edu.br>
2025-09-10 14:30:08 -0300

    TelaLogin

--- PDS/src/Atividade1/TelaLogin.java
+++ PDS/src/Atividade1/TelaLogin.java
@@ -1,33 +1,26 @@
 package Atividade1;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.text.AbstractDocument;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DocumentFilter;
-
-import net.miginfocom.swing.MigLayout;
-
-
-import java.awt.Color;
-import javax.swing.JTextField;
-import javax.swing.JTextPane;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-
-import java.awt.Font;
-import javax.swing.JCheckBox;
-import javax.swing.JRadioButton;
-import javax.swing.JButton;
-
 import javax.swing.text.MaskFormatter;
-import javax.swing.JFormattedTextField;
+import javax.swing.text.DocumentFilter.FilterBypass;
 
-public class TelaCadastro extends JFrame {
+public class TelaLogin extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -41,7 +34,7 @@ public class TelaCadastro extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					TelaCadastro frame = new TelaCadastro();
+					TelaLogin frame = new TelaLogin();
 					frame.setVisible(true);
 					System.out.println("dsfsdf");
 				} catch (Exception e) {
@@ -55,11 +48,16 @@ public class TelaCadastro extends JFrame {
 	    dispose(); // Fecha a tela atual
 	    new TelaCadastroProdutos().setVisible(true); // Abre a tela de cadastro
 	}
+	
+	private void abrirTelaCadastro() {
+	    dispose(); // Fecha a tela atual
+	    new TelaCadastro().setVisible(true); // Abre a tela de cadastro
+	}
 	/**
 	 * Create the frame.
 	 */
-	public TelaCadastro() {
-		setTitle("Cadastro");
+	public TelaLogin() {
+		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -75,7 +73,7 @@ public class TelaCadastro extends JFrame {
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNome = new JLabel("Nome");
-		lblNome.setBounds(10, 59, 39, 19);
+		lblNome.setBounds(57, 82, 39, 19);
 		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		contentPane.add(lblNome);
 		
@@ -98,7 +96,7 @@ public class TelaCadastro extends JFrame {
 		}
 		
 		txtNome = new JTextField();
-		txtNome.setBounds(56, 58, 240, 23);
+		txtNome.setBounds(121, 80, 240, 23);
 		txtNome.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		contentPane.add(txtNome);
 		txtNome.setColumns(10);
@@ -108,7 +106,7 @@ public class TelaCadastro extends JFrame {
 		
 		
 		JLabel lblCPF = new JLabel("CPF");
-		lblCPF.setBounds(12, 102, 25, 19);
+		lblCPF.setBounds(59, 132, 25, 19);
 		lblCPF.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		contentPane.add(lblCPF);
 		
@@ -121,49 +119,40 @@ public class TelaCadastro extends JFrame {
 		}
 
 		JFormattedTextField txtCPF = new JFormattedTextField(cpfMask);
-		txtCPF.setBounds(55, 102, 98, 23);
+		txtCPF.setBounds(121, 130, 98, 23);
 		txtCPF.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		contentPane.add(txtCPF);
-		
-		JCheckBox chckbxADM = new JCheckBox("Sou Administrador");
-		chckbxADM.setBounds(50, 146, 146, 30);
-		chckbxADM.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		contentPane.add(chckbxADM);
 		
 		JButton btnEntrar = new JButton("Entrar");
 		btnEntrar.setBounds(272, 197, 89, 30);
 		btnEntrar.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		contentPane.add(btnEntrar);
-		
+
 		btnEntrar.addActionListener(e -> {
 		    String nome = txtNome.getText();
 		    String cpf = txtCPF.getText();
-		    boolean isAdmin = chckbxADM.isSelected();
 
 		    if (nome.isEmpty() || cpf.contains("_")) {
 		        JOptionPane.showMessageDialog(this, "Preencha todos os campos corretamente!");
 		        return;
 		    }
 
-		    dispose(); // fecha a tela atual
-
-		    if (isAdmin) {
-		        new TelaCadastroProdutos().setVisible(true);
-		    } else {
-		        new TelaCompra().setVisible(true);
-		    }
+		    dispose(); // Fecha a tela atual
+		    new TelaCadastroProdutos().setVisible(true); // Vai para TelaCadastro
 		});
 		
 		
 		
-		JButton btnCancelar = new JButton("Cancelar");
-		btnCancelar.setBounds(78, 197, 89, 30);
-		btnCancelar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		contentPane.add(btnCancelar);
+		JButton btnMeCadastar = new JButton("Me Cadastrar");
+		btnMeCadastar.setBounds(78, 197, 130, 30);
+		btnMeCadastar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		contentPane.add(btnMeCadastar);
 
-		btnCancelar.addActionListener(e -> {
-		    System.exit(0); // fecha o sistema
+		btnMeCadastar.addActionListener(e -> {
+		    dispose(); // Fecha a tela atual
+		    new TelaCadastro().setVisible(true); // Vai para TelaCadastro
 		});
-
+		
 	}
 }
+
